f37720984f86e72756e03298750bafdea707c557
Bryan Schreuder <bryanschreuder91@gmail.com>
2017-12-20 14:48:31 +0100

    Added rabbit props

--- Framework/SDLFramework/SDLFramework/Rabbit.h
+++ Framework/SDLFramework/SDLFramework/Rabbit.h
@@ -1,10 +1,45 @@
 #pragma once
-
+#include "Vector.h"
 //FORCE DRIVEN ENTITY
 class Rabbit
 {
 public:
 	Rabbit();
 	~Rabbit();
+
+	
+
+private:
+	//functions
+	
+	//wander()
+	
+
+	//Properties
+	/*
+		Cohesion (bij de groep blijven)
+		Seperation (
+	*/
+	float cohesion; //Bij de groep blijven 0 tot 1.0
+	float seperation; //Uit de buurt van anderen blijven 0 tot 1.0
+	float alignment; //Zelfde kant als zijn buren 0 tot 1.0
+
+	float dogAttract; //Aantrekking tot dog -1.0 tot 1.0
+	float waterAttract; //Aantrekking tot water -1.0 tot 1.0
+
+
+	//Forces
+	Vector m_Velocity;
+	Vector m_Heading;
+	Vector m_Side;
+	
+	float m_Mass;
+	float m_MaxSpeed;
+	float m_MaxForce;
+	float m_MaxTurnrate;
+
+	//for drawing
+	int width = 5;
+	int height = 5;
 };
 
